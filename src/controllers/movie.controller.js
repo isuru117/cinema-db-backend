6661d1e966be9d49b8cd25eeac77ae1de6a621aa
isuru117 <isuruedirisinghe80@gmail.com>
2021-08-31 02:36:55 +0530

    add get movies

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,3 +1,17 @@
-import movieModel from "../models/movie.model";
+import MovieModel from "../models/movie.model";
+import StatusCodes from 'http-status-codes';
 
-export const read = () => movieModel.find();
+const MovieController = {}
+
+MovieController.search = async (req, res) => {
+    MovieModel.find({ "name": { $regex: '.*' + req.query.query + '.*' } }).then((data) => {
+        if (data) {
+            res.status(StatusCodes.OK).send(data);
+        }
+
+    }).catch(()=>{
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+    });
+}
+
+export default MovieController;
