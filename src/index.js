1c84c4e1975c13b29e79a28b6d6718732d965bab
isuru117 <isuruedirisinghe80@gmail.com>
2021-09-04 02:56:57 +0530

    add swagger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import helmet from 'helmet';
-import config from './config/config.js';
-import movieRoutes from './routes/movie.routes.js';
 import mongoose from 'mongoose';
+import swaggerUi from 'swagger-ui-express';
+
+import config from './config/main.config.js';
+import movieRoutes from './routes/movie.routes.js';
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const swaggerDocument = require('./../swagger.json');
 
 const app = express();
 
@@ -13,20 +19,21 @@ mongoose.Promise = global.Promise;
 mongoose.connect(config.mongo_url, {
     useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet.contentSecurityPolicy());
 app.use(helmet.referrerPolicy());
 
-app.use('/api', movieRoutes);
+app.use('/', movieRoutes);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.1/themes/3.x/theme-feeling-blue.css" }));
 
-function onStart(){
+function onStart() {
     console.log(`Server running on port ${PORT}`);
 }
 
